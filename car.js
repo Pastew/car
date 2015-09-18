18c4ff82011578f36eb378506079a148bf44831c
Pastew <kpastew@gmail.com>
2015-09-18 20:29:35 +0200

    speed control

--- car.js
+++ car.js
@@ -1,14 +1,16 @@
-//var exports = module.exports = {};
+var status = "==========";
 
+// ========== constans ==========
 var HIGH = 1;
 var LOW = 0;
+MIN_SPEED = 0;
+MAX_SPEED = 250
+SPEED_SPAN = 10;
+leftSpeed = 255;
+rightSpeed = 255;
 
-/**** Galileo-io version ****/
-
-var status = "---";
-
+// ========== pins ==========
 ledNumber = 8;
-
 L1 = 10;
 L2 = 9;
 R1 = 6;
@@ -17,95 +19,156 @@ R2 = 5;
 UP_DOWN = 11;
 OPEN_CLOSE = 3;
 
-/*
- * About controlling galileo with a webpage
- */
+
+// ========== action to function mapping ==========
 module.exports = {
-  handleAction : function(action){
-    console.log("CAR action: " + action)
+    handleAction: function (action) {
+        console.log("CAR action: " + action)
 
-    if(action === "ledOn") turnOnLed();
-    else if(action === "ledOff") turnOffLed();
-    else if(action === "forward") forward();
-    else if(action === "backward") backward();
-    else if(action === "left") left();
-    else if(action === "right") right();
-    else if(action === "stop") stop();
-    else if(action === "up") up();
-    else if(action === "down") down();
-    else if(action === "open") open();
-    else if(action === "close") close();
-    else if(action === "stream") streamingOn();
-    else console.log("Unexpected request action: " + action);
-  }
+        if (action === "ledOn") turnOnLed();
+        else if (action === "ledOff") turnOffLed();
+        else if (action === "forward") forward();
+        else if (action === "backward") backward();
+        else if (action === "left") left();
+        else if (action === "right") right();
+        else if (action === "stop") stop();
+        else if (action === "up") up();
+        else if (action === "down") down();
+        else if (action === "open") open();
+        else if (action === "close") close();
+        else if (action === "both_speed_plus") both_speed_plus();
+        else if (action === "both_speed_minus") both_speed_minus();
+        else if (action === "left_speed_minus") left_speed_minus();
+        else if (action === "left_speed_plus") left_speed_plus();
+        else if (action === "right_speed_minus") right_speed_minus();
+        else if (action === "right_speed_plus") right_speed_plus();
+        else console.log("Unexpected request action: " + action);
+    }
 }
 
-function forward(){
-  board.digitalWrite(L1, HIGH);
-  board.digitalWrite(R1, HIGH);
-  board.digitalWrite(L2, LOW);
-  board.digitalWrite(R2, LOW);
-  status = "forward";
+// ========== steering ==========
+function forward() {
+    board.analogWrite(L1, leftSpeed);
+    board.analogWrite(R1, rightSpeed);
+    board.analogWrite(L2, 0);
+    board.analogWrite(R2, 0);
+    status = "forward";
 }
 
-function backward(){
-  board.digitalWrite(L1, LOW);
-  board.digitalWrite(R1, LOW);
-  board.digitalWrite(L2, HIGH);
-  board.digitalWrite(R2, HIGH);
-  status = "back";
+function backward() {
+    board.analogWrite(L1, 0);
+    board.analogWrite(R1, 0);
+    board.analogWrite(L2, leftSpeed);
+    board.analogWrite(R2, rightSpeed);
+    status = "back";
 }
 
-function right(){
-  board.digitalWrite(L1, HIGH);
-  board.digitalWrite(R1, LOW);
-  board.digitalWrite(L2, LOW);
-  board.digitalWrite(R2, HIGH);
-  status = "right";
+function right() {
+    board.analogWrite(L1, leftSpeed);
+    board.analogWrite(R1, 0);
+    board.analogWrite(L2, 0);
+    board.analogWrite(R2, rightSpeed);
+    status = "right";
 }
 
-function left(){
-  board.digitalWrite(L1, LOW);
-  board.digitalWrite(R1, HIGH);
-  board.digitalWrite(L2, HIGH);
-  board.digitalWrite(R2, LOW);
-  status = "left";
+function left() {
+    board.analogWrite(L1, 0);
+    board.analogWrite(R1, rightSpeed);
+    board.analogWrite(L2, leftSpeed);
+    board.analogWrite(R2, 0);
+    status = "left";
 }
 
-function stop(){
-  board.digitalWrite(L1, LOW);
-  board.digitalWrite(R1, LOW);
-  board.digitalWrite(L2, LOW);
-  board.digitalWrite(R2, LOW);
-  status = "stop";
+function stop() {
+    board.analogWrite(L1, 0);
+    board.analogWrite(R1, 0);
+    board.analogWrite(L2, 0);
+    board.analogWrite(R2, 0);
+    status = "stop";
 }
 
-function up(){
-  board.servoWrite(UP_DOWN, 100);
+// ========== motors speed ==========
+function both_speed_plus() {
+    if(leftSpeed + SPEED_SPAN > MAX_SPEED
+    || rightSpeed + SPEED_SPAN > MAX_SPEED)
+        return -1;
+    
+    left_speed_plus();
+    right_speed_plus();
 }
 
-function down(){
-  board.servoWrite(UP_DOWN, 50);
+function both_speed_minus() {
+    if(leftSpeed - SPEED_SPAN < MIN_SPEED
+    || rightSpeed - SPEED_SPAN < MIN_SPEED)
+        return -1;
+    
+    left_speed_minus();
+    right_speed_minus();
 }
 
-function close(){
-  board.servoWrite(OPEN_CLOSE, 80);
+function left_speed_minus() {
+    leftSpeed -= SPEED_SPAN;
+    if (leftSpeed < MIN_SPEED)
+        leftSpeed = MIN_SPEED;
+    
+    //$("#left-speed").text(formatSpeed(leftSpeed));
 }
 
-function open(){
-  board.servoWrite(OPEN_CLOSE, 0);
+function left_speed_plus() {
+    leftSpeed += SPEED_SPAN;
+    if (leftSpeed > MAX_SPEED)
+        leftSpeed = MAX_SPEED;
+    
+    //$("#left-speed").text(formatSpeed(leftSpeed));
 }
 
-function turnOnLed(){
-  board.digitalWrite(ledNumber, HIGH);
+function right_speed_minus() {
+    rightSpeed -= SPEED_SPAN;
+    if (rightSpeed < MIN_SPEED)
+        rightSpeed = MIN_SPEED;
+    
+    //$("#right-speed").text(formatSpeed(rightSpeed));
 }
 
-function turnOffLed(){
-  board.digitalWrite(ledNumber, LOW);
+function right_speed_plus() {
+    rightSpeed += SPEED_SPAN;
+    if (rightSpeed > MAX_SPEED)
+        rightSpeed = MAX_SPEED; 
+    
+    //$("#right-speed").text(formatSpeed(rightSpeed));
 }
 
-function streamingOn(){
-  console.log("Turning on video stream...");
-  exec("/home/root/websockets/stream.sh", function(err, result) {
-  });
+function formatSpeed(s){
+    return String(round((s-MIN_SPEED) / (MAX_SPEED-MIN_SPEED) * 100));
+}
+
+// ========== servo ==========
+function up() {
+    board.servoWrite(UP_DOWN, 100);
+}
+
+function down() {
+    board.servoWrite(UP_DOWN, 50);
+}
+
+function close() {
+    board.servoWrite(OPEN_CLOSE, 80);
+}
+
+function open() {
+    board.servoWrite(OPEN_CLOSE, 0);
+}
+
+function turnOnLed() {
+    board.digitalWrite(ledNumber, HIGH);
+}
+
+function turnOffLed() {
+    board.digitalWrite(ledNumber, LOW);
+}
+
+// ========== streaming ==========
+function streamingOn() {
+        console.log("Turning on video stream...");
+        exec("/home/root/websockets/stream.sh", function (err, result) {});
 }
--- index.js
+++ index.js
@@ -5,36 +5,37 @@ var io = require('socket.io')(http);
 var Galileo = require("galileo-io");
 exec = require("child_process").exec;
 var car = require('./car.js');
-    $ = require('jquery');
-    board = new Galileo();
+$ = require('jquery');
+board = new Galileo();
 
-    app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket) {
-  console.log("Ktos przyszedl");
-  socket.on('action', function(action) {
-    car.handleAction(action);
-  });
+io.on('connection', function (socket) {
+    console.log("Ktos przyszedl");
+    socket.on('action', function (action) {
+        car.handleAction(action);
+    });
 });
 
-http.listen(3000, function() {
-  console.log('listening on port :3000');
-  console.log('Mozna jezdzic!');
+http.listen(3000, function () {
+    console.log('listening on port :3000');
+    console.log('Drive!');
 });
 
-board.on("ready", function() {
-  console.log("Board is ready");
-  this.pinMode(ledNumber, this.MODES.OUTPUT);
+board.on("ready", function () {
+    console.log("Board is ready");
+    console.log("Setting up pins...");
+    this.pinMode(ledNumber, this.MODES.OUTPUT);
 
-  this.pinMode(L1, this.MODES.OUTPUT);
-  this.pinMode(L2, this.MODES.OUTPUT);
-  this.pinMode(R1, this.MODES.OUTPUT);
-  this.pinMode(R2, this.MODES.OUTPUT);
-  this.pinMode(UP_DOWN, this.MODES.OUTPUT);
-  this.pinMode(OPEN_CLOSE, this.MODES.OUTPUT);
-  console.log("Output Pins setup completed");
+    this.pinMode(L1, this.MODES.OUTPUT);
+    this.pinMode(L2, this.MODES.OUTPUT);
+    this.pinMode(R1, this.MODES.OUTPUT);
+    this.pinMode(R2, this.MODES.OUTPUT);
+    this.pinMode(UP_DOWN, this.MODES.OUTPUT);
+    this.pinMode(OPEN_CLOSE, this.MODES.OUTPUT);
+    console.log("Output Pins setup completed");
 });
